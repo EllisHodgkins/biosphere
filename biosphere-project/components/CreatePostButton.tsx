753969d32f9dae6153f1fb3de39fb5e65013ae72
Callum Beever <callumbeever@Callums-MacBook-Air.local>
2022-03-22 14:08:04 +0000

    broken navigation

---
+++ biosphere-project/components/CreatePostButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const CreatePostButton: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+});
+
+export default CreatePostButton;
---
+++ biosphere-project/components/Map.tsx
@@ -0,0 +1,78 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+import * as React from 'react';
+import * as Location from 'expo-location';
+import { useState, useEffect } from 'react';
+import CreatePostButton from './CreatePostButton';
+
+interface LocationState {
+  latitude: number;
+  longitude: number;
+}
+
+const MainMap: React.FC = () => {
+  const [location, setLocation] = useState<LocationState | {}>({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    Location.requestForegroundPermissionsAsync()
+      .then(({ status }) => {
+        if (status !== 'granted') {
+          console.log('error - this is not working');
+          return;
+        }
+
+        return Location.getCurrentPositionAsync({});
+      })
+      .then((location) => {
+        setLocation(location.coords);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (isLoading)
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+        followsUserLocation={true}
+        showsCompass={true}
+        userLocationUpdateInterval={60000}
+        userLocationFastestInterval={60000}
+      >
+        <CreatePostButton />
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+export default MainMap;
