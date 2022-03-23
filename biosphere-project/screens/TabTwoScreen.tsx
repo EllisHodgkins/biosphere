0ccc609a9b4d3eab355a0da0429503172ca1efca
Callum Beever <callumbeever@Callums-MacBook-Air.local>
2022-03-23 21:44:52 +0000

    fix type errs in camera function

--- biosphere-project/screens/TabTwoScreen.tsx
+++ biosphere-project/screens/TabTwoScreen.tsx
@@ -1,45 +1,44 @@
-import { ToastAndroid } from "react-native";
-import { Text, View } from "../components/Themed";
-import * as ImagePicker from "expo-image-picker";
-import { useEffect, useState } from "react";
+import { ToastAndroid } from 'react-native';
+import { Text, View } from '../components/Themed';
+import * as ImagePicker from 'expo-image-picker';
+import { useEffect, useState } from 'react';
 
+interface Props {
+  navigation: any;
+}
 
-const TakePhoto: React.FC = ({navigation}) => {
+const TakePhoto: React.FC<Props> = ({ navigation }) => {
   const [image, setImage] = useState({});
 
   useEffect(() => {
-    return ImagePicker.requestCameraPermissionsAsync()
-    .then((permissionResponse) => {
-      if (!permissionResponse) {
-        ToastAndroid.show(
-          "Please allow camera permissions in settings",
-          ToastAndroid.LONG
-        );
-        return { cancelled: true };
-      }
-      return ImagePicker.launchCameraAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true,
-        aspect: [1, 1],
-        quality: 1,
-        base64: true,
+    ImagePicker.requestCameraPermissionsAsync()
+      .then((permissionResponse) => {
+        if (!permissionResponse) {
+          ToastAndroid.show(
+            'Please allow camera permissions in settings',
+            ToastAndroid.LONG
+          );
+          return { cancelled: true };
+        }
+        return ImagePicker.launchCameraAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
+          base64: true,
+        });
+      })
+      .then((result) => {
+        if (!result.cancelled) {
+          setImage(result);
+          navigation.goBack();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    })
-    .then((result) => {
-      if (!result.cancelled) {
-        setImage(result);
-        navigation.goBack()
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }, [])
-  return (
-    <View>
-      <Text>Oops...</Text>
-    </View>
-  )
-}
+  }, []);
+  return <></>;
+};
 
 export default TakePhoto;
