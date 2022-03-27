fbaa7732d497feeaa108d0beaf8b421adf91acb6
Thomas Butterworth <thomas.m.butterworth@gmail.com>
2022-03-27 02:54:22 +0100

    modal form laid out

--- biosphere-project/screens/ModalScreen.tsx
+++ biosphere-project/screens/ModalScreen.tsx
@@ -1,14 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useLayoutEffect, useState } from "react";
 import {
   Platform,
   StyleSheet,
   TextInput,
   Pressable,
   Image,
-} from 'react-native';
-import { Text, View } from '../components/Themed';
-import { useForm, Controller } from 'react-hook-form';
+} from "react-native";
+import { Text, View } from "../components/Themed";
+import { useForm, Controller } from "react-hook-form";
+import CustomMultiPicker from "react-native-multiple-select-list";
+
+const tagList = ["Invasive", "Plant", "Weed", "Indigenous", "Planted", "Wild"]
 
 interface Props {
   navigation: any;
@@ -22,12 +25,12 @@ const ModalScreen: React.FC<Props> = ({ navigation, route }) => {
     formState: { errors },
   } = useForm({
     defaultValues: {
-      firstName: '',
-      lastName: '',
+      firstName: "",
+      lastName: "",
     },
   });
 
-  const [photo, setPhoto] = useState<string>('');
+  const [photo, setPhoto] = useState<string>("");
 
   useEffect(() => {
     if (route.params) {
@@ -40,8 +43,8 @@ const ModalScreen: React.FC<Props> = ({ navigation, route }) => {
       headerLeft: () => (
         <Pressable
           onPress={() =>
-            navigation.navigate('Root', {
-              screen: 'MapPage',
+            navigation.navigate("Root", {
+              screen: "MapPage",
             })
           }
           style={styles.backButton}
@@ -52,18 +55,48 @@ const ModalScreen: React.FC<Props> = ({ navigation, route }) => {
 
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} placeholder="input test" />
+      <TextInput style={styles.input} placeholder="Title" />
       {photo ? (
         <Image style={styles.userImage} source={{ uri: photo }} />
       ) : null}
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
 
+      <View style={styles.formContainer}>
+
+      <CustomMultiPicker
+        options={tagList}
+        search={true} // should show search bar?
+        multiple={true} //
+        placeholder={"Add a Tag"}
+        placeholderTextColor={"#757575"}
+        returnValue={"label"} // label or value
+        callback={(res) => {
+          console.log(res);
+        }} // callback, array of selected items
+        rowBackgroundColor={"#eee"}
+        rowHeight={40}
+        rowRadius={5}
+        searchIconName="ios-checkmark"
+        searchIconColor="blakc"
+        searchIconSize={25}
+        iconColor={"#00a2dd"}
+        iconSize={15}
+        selectedIconName={"ios-checkmark-circle-outline"}
+        unselectedIconName={"ios-radio-button-off-outline"}
+        scrollViewHeight={100}
+      />
+      </View>
+
+      <TextInput style={styles.paraInput} placeholder="Optional Description"/>
+      <Pressable
+          onPress={() =>
+            navigation.navigate("Root", {
+              screen: "MapPage",
+            })
+          }
+          style={styles.submitButton}
+        ><Text>Upload</Text></Pressable>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
 };
@@ -71,34 +104,54 @@ const ModalScreen: React.FC<Props> = ({ navigation, route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
   backButton: {
     width: 50,
     height: 30,
-    backgroundColor: 'green',
+    backgroundColor: "green",
   },
   userImage: {
-    flex: 1,
-    width: 350,
-    height: 350,
+    width: 250,
+    height: 250,
   },
   input: {
     height: 40,
+    width: 250,
     margin: 12,
     borderWidth: 1,
     padding: 10,
   },
+  paraInput: {
+    height: 150,
+    width: 250,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  formContainer: {
+    width: 250,
+  },
+  submitButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 100,
+    height: 45,
+    borderRadius: 75,
+    borderColor: "black",
+    borderWidth: 3,
+
+  }
 });
 
 export default ModalScreen;
